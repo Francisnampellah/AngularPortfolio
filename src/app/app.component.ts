b49bdfcf6e45c4c5572428cd04100ed330be9a2b
francisnampellah <bnampellah1@gmail.com>
2024-10-31 23:47:35 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientCardComponent } from './component/Clientcards/clientCard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ClientCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello_world';
+  clients = [
+    {
+      "clientName": "Baraka Nampellah",
+      "clientTitle": "Software Engineer",
+      "clientTestimony": "The project is exciting!",
+      "clientImage": "https://images.unsplash.com/photo-1520528105264-de3db89485f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "clientName": "Alex Johnson",
+      "clientTitle": "Product Manager",
+      "clientTestimony": "Impressive results and great collaboration!",
+      "clientImage": "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "clientName": "Sam Lee",
+      "clientTitle": "UX Designer",
+      "clientTestimony": "A fantastic user experience throughout!",
+      "clientImage": "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+  ]
 
-  words = ['analyse', 'advise', 'manage'];
+
+  @ViewChild('clientCardContainer', { static: false }) clientCardContainer!: ElementRef;
+
+  scrollLeft() {
+    this.clientCardContainer.nativeElement.scrollBy({ left: -200, behavior: 'smooth' });
+  }
+
+  scrollRight() {
+    this.clientCardContainer.nativeElement.scrollBy({ left: 200, behavior: 'smooth' });
+  }
 }
