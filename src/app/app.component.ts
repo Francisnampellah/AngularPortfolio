e66ef3b2f59d87b26c2b08b354331279488a0431
francisnampellah <bnampellah1@gmail.com>
2024-11-01 21:26:29 +0300

    i amaze myself

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientCardComponent } from './component/Clientcards/clientCard.component';
+import { WorkCardComponent } from './component/WorkCard/workCard.component';
+import { LottieAnimationComponent } from './component/lottie/lottie.component';
+
+// Export this function
+export function playerFactory(): any {
+  return import('lottie-web');
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ClientCardComponent],
+  imports: [CommonModule, ClientCardComponent, WorkCardComponent, LottieAnimationComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -30,6 +37,61 @@ export class AppComponent {
       "clientImage": "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }
   ]
+
+
+  contacts = [
+    {
+      type: "email",
+      value: "bnampellah1@gmail.com"
+    },
+    {
+      type: "telephone",
+      value: "+1234567890"
+    },
+    {
+      type: "whatsapp",
+      value: "+0987654321"
+    },
+    {
+      type: "github",
+      value: "https://github.com/username"
+    }
+  ];
+
+  links = [
+    {
+      linkText: "Home",
+      linkUrl: "#"
+    },
+    {
+      linkText: "About",
+      linkUrl: "#"
+    },
+    {
+      linkText: "Services",
+      linkUrl: "#"
+    },
+    {
+      linkText: "Contact",
+      linkUrl: "#"
+    }
+  ];
+
+  workCards = [
+    {
+      workButton: "Default Name",
+      workTitle: "Default Title",
+      workDetail: "Default Testimony",
+      workImage: "Default Image"
+    },
+    {
+      workButton: "Another Name",
+      workTitle: "Another Title",
+      workDetail: "Another Testimony",
+      workImage: "Another Image"
+    }
+  ];
+
 
 
   @ViewChild('clientCardContainer', { static: false }) clientCardContainer!: ElementRef;
